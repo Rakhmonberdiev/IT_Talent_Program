8fef6728c1f9e60784ef80cd413a0cc1bf3bf4c4
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-13 00:16:44 +0500

    AppServiceExtensions

---
+++ IT_Talent_Program/Extensions/AppServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using IT_Talent_Program.Data;
+using IT_Talent_Program.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT_Talent_Program.Extensions
+{
+    public static class AppServiceExtensions
+    {
+        public static IServiceCollection AddAppService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlite(configuration.GetConnectionString("Sql"));
+            });
+            services.AddScoped<ITokenGen, TokenGen>();
+
+            return services;
+        }
+    }
+}
--- IT_Talent_Program/Program.cs
+++ IT_Talent_Program/Program.cs
@@ -1,6 +1,10 @@
+using IT_Talent_Program.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAppService(builder.Configuration);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
