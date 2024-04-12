fde585860c931a7e11d78f650b14148a188813f3
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-12 22:49:37 +0500

    Dbcontext

---
+++ IT_Talent_Program/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IT_Talent_Program.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT_Talent_Program.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
