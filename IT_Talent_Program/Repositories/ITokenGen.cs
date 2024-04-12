6563e7212a1855e7034c5426b15d96e69cd8bf1e
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-12 23:59:46 +0500

    Itokengen

---
+++ IT_Talent_Program/Repositories/ITokenGen.cs
@@ -0,0 +1,9 @@
+﻿using IT_Talent_Program.Entities;
+
+namespace IT_Talent_Program.Repositories
+{
+    public interface ITokenGen
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ IT_Talent_Program/Repositories/TokenGen.cs
@@ -0,0 +1,38 @@
+﻿using IT_Talent_Program.Data;
+using IT_Talent_Program.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IT_Talent_Program.Repositories
+{
+    public class TokenGen : ITokenGen
+    {
+        private readonly SymmetricSecurityKey _key;
+        public TokenGen(IConfiguration configuration)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+        }
+        public string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim (JwtRegisteredClaimNames.UniqueName, user.Login)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
