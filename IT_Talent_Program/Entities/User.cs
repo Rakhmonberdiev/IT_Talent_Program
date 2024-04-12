9be3463df4b73d1c209578f2b198d112864c3e0a
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-12 22:41:58 +0500

    User entity

---
+++ IT_Talent_Program/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace IT_Talent_Program.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public DateTime? Birthday { get; set; }
+        public bool Admin { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedOn { get; set;}
+        public string ModifiedBy { get; set;}
+        public DateTime RevokedOn { get; set; }
+        public string RevokedBy { get; set;}
+    }
+}
