4ed866cd7c21ea799145bfc4ccad0538299823b9
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-13 01:07:37 +0500

    IUserRepo

---
+++ IT_Talent_Program/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using IT_Talent_Program.Entities;
+
+namespace IT_Talent_Program.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByLogin(string login);
+        Task<User> Create(User user);
+    }
+}
---
+++ IT_Talent_Program/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using IT_Talent_Program.Data;
+using IT_Talent_Program.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT_Talent_Program.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db;
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<User> Create(User user)
+        {
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> GetUserByLogin(string login)
+        {
+            return await _db.Users.SingleOrDefaultAsync(x => x.Login == login);
+        }
+    }
+}
